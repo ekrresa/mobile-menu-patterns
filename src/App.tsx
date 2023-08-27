9da5f4a5e21b6d49bdb0e4aa1d62a571aad2df82
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-08-27 17:57:54 +0100

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,26 @@ import { PushContentDown } from './components/PushContentDown'
 function App() {
   return (
     <div className="grid h-screen grid-rows-[auto,auto,1fr] justify-center">
-      <h1 className="mb-2 pb-3 pt-4 text-center text-3xl text-[#2C4251]">
+      <h1 className="mb-2 pb-3 pt-4 text-center text-3xl font-medium text-baltic-900">
         Mobile Menu Patterns
       </h1>
       <div className="mb-8 flex max-w-2xl flex-wrap justify-center gap-4">
-        <button className="rounded-md bg-mosaic-500 px-4 py-1 capitalize text-gray-50">
+        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
           Push content down
         </button>
-        <button className="rounded-md bg-mosaic-500 px-4 py-1 capitalize text-gray-50">
+        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
           fullscreen overlay
         </button>
-        <button className="rounded-md bg-mosaic-500 px-4 py-1 capitalize text-gray-50">
+        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
           pop-over panel
         </button>
-        <button className="rounded-md bg-mosaic-500 px-4 py-1 capitalize text-gray-50">
+        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
           slide-in from left
         </button>
-        <button className="rounded-md bg-mosaic-500 px-4 py-1 capitalize text-gray-50">
+        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
           slide-in from right
         </button>
-        <button className="rounded-md bg-mosaic-500 px-4 py-1 capitalize text-gray-50">
+        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
           slide-in from bottom
         </button>
       </div>
--- src/components/PushContentDown.tsx
+++ src/components/PushContentDown.tsx
@@ -39,7 +39,7 @@ export function PushContentDown() {
     <div className="mx-auto mb-4 w-full max-w-md overflow-y-auto rounded-md bg-white text-baltic-900 drop-shadow">
       <div className="sticky top-0 bg-white">
         <div className="flex items-center justify-between border-b px-4 py-4">
-          <h1 className="text-2xl font-semibold">Contacts</h1>
+          <h1 className="text-2xl font-medium">Contacts</h1>
 
           <button
             className="grid place-items-center"
@@ -76,7 +76,11 @@ export function PushContentDown() {
 
         <div className="border-b bg-mosaic-50 px-4 py-4">
           <div className="flex items-center overflow-hidden rounded border bg-white pl-3 pr-2">
-            <Search className="text-xl text-slate-500" />
+            <Search
+              className="text-xl text-slate-500"
+              size={20}
+              strokeWidth={1.5}
+            />
             <input
               className="ml-3 flex-1 py-2 font-normal text-slate-800 focus:outline-none"
               onChange={e => setSearchString(e.target.value)}
@@ -94,7 +98,7 @@ export function PushContentDown() {
             return (
               <React.Fragment key={alphabet}>
                 <div className="border-y bg-mosaic-50 px-4 uppercase">
-                  <p className="w-12 text-center">{alphabet}</p>
+                  <p className="w-10 text-center">{alphabet}</p>
                 </div>
 
                 {(contacts ?? []).map(contact => (
@@ -102,16 +106,16 @@ export function PushContentDown() {
                     key={contact.id}
                     className="flex items-center gap-4 border-b px-4 py-4"
                   >
-                    <span className="flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-mosaic-100">
+                    <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-mosaic-100">
                       {contact.first_name[0]}
                       {contact.last_name[0]}
                     </span>
 
                     <div className="">
-                      <p className="text-lg font-medium capitalize">
+                      <p className=" font-medium capitalize">
                         {contact.first_name + ' ' + contact.last_name}
                       </p>
-                      <p className="font-normal tabular-nums">
+                      <p className="text-sm font-normal tabular-nums">
                         {contact.phone_number}
                       </p>
                     </div>
