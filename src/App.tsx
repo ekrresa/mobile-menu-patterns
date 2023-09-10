ff3991481f5850da70e88b6ba6585119f67888c9
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-09-10 14:20:06 +0100

    Redesign pattern links

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,97 @@
-// import { PushContentDown } from './components/PushContentDown'
-import { SlideInFromBottom } from './components/SlideInFromBottom'
+import * as React from 'react'
 
+import { FullScreenOverlay } from './components/FullScreenOverlay'
+import { PopOverPanel } from './components/PopOverPanel'
+import { PushContentDown } from './components/PushContentDown'
+import { SlideInFromBottom } from './components/SlideInFromBottom'
+import { SlideInFromLeft } from './components/SlideInFromLeft'
+import { SlideInFromRight } from './components/SlideInFromRight'
+
+type PatternState =
+  | 'push-content-down'
+  | 'slide-in-from-bottom'
+  | 'fullscreen-overlay'
+  | 'pop-over-panel'
+  | 'slide-in-from-left'
+  | 'slide-in-from-right'
+
+const patternMap: Record<PatternState, React.ReactNode> = {
+  'push-content-down': <PushContentDown />,
+  'slide-in-from-bottom': <SlideInFromBottom />,
+  'fullscreen-overlay': <FullScreenOverlay />,
+  'pop-over-panel': <PopOverPanel />,
+  'slide-in-from-left': <SlideInFromLeft />,
+  'slide-in-from-right': <SlideInFromRight />,
+}
 function App() {
+  const [activePattern, setActivePattern] =
+    React.useState<PatternState>('push-content-down')
+
   return (
-    <div className="grid h-screen grid-rows-[auto,auto,1fr] justify-center">
-      <h1 className="mb-2 pb-3 pt-4 text-center text-3xl font-medium text-baltic-900">
-        Mobile Menu Patterns
-      </h1>
-      <div className="mb-8 flex max-w-2xl flex-wrap justify-center gap-4">
-        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
+    <div className="mx-auto max-w-3xl px-6 text-center">
+      <header>
+        <h1 className="text-center text-[2.5rem] font-semibold text-baltic-950">
+          Mobile Menu Patterns
+        </h1>
+
+        <p className="mt-2 text-center text-lg text-baltic-900">
+          A collection of patterns for site navigation on mobile
+        </p>
+      </header>
+
+      <section className="mb-8 mt-24 flex flex-wrap justify-center gap-x-4 gap-y-10 text-left">
+        <button
+          className="border-b-4 border-baltic-700 bg-transparent text-lg capitalize text-baltic-700"
+          onClick={() => setActivePattern('fullscreen-overlay')}
+        >
+          Full Screen Overlay
+        </button>
+        <button
+          className="border-b-4 border-baltic-700 bg-transparent text-lg capitalize text-baltic-700"
+          onClick={() => setActivePattern('pop-over-panel')}
+        >
+          Popover Panel
+        </button>
+        <button
+          className="border-b-4 border-baltic-700 bg-transparent text-lg capitalize text-baltic-700"
+          onClick={() => setActivePattern('push-content-down')}
+        >
           Push content down
         </button>
-        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
-          fullscreen overlay
+        <button
+          className="border-b-4 border-baltic-700 bg-transparent text-lg capitalize text-baltic-700"
+          onClick={() => setActivePattern('slide-in-from-bottom')}
+        >
+          Slide in from bottom
         </button>
-        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
-          pop-over panel
+        <button
+          className="border-b-4 border-baltic-700 bg-transparent text-lg capitalize text-baltic-700"
+          onClick={() => setActivePattern('slide-in-from-left')}
+        >
+          Slide in from left
         </button>
-        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
-          slide-in from left
-        </button>
-        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
-          slide-in from right
-        </button>
-        <button className="rounded-md bg-mosaic-600 px-4 py-1 capitalize text-gray-50">
-          slide-in from bottom
-        </button>
-      </div>
 
-      <SlideInFromBottom />
+        <button
+          className="border-b-4 border-baltic-700 bg-transparent text-lg capitalize text-baltic-700"
+          onClick={() => setActivePattern('slide-in-from-right')}
+        >
+          Slide in from right
+        </button>
+      </section>
+
+      {/* {patternMap[activePattern]} */}
+
+      <footer className="absolute bottom-0 left-0 mt-8 w-full py-4 text-center text-sm text-baltic-700">
+        Built by{' '}
+        <a
+          href="https://github.com/ekrresa"
+          target="_blank"
+          rel="noreferrer"
+          className="decoration-slate-500 transition-all duration-500 hover:underline"
+        >
+          Ochuko
+        </a>
+      </footer>
     </div>
   )
 }
